c3a5041f3c0e977853354455ca6b9e7687d566eb
Vanh Souvanlasy <afsmvan@atlantis>
2009-08-20 10:59:19 -0400

    Add a test for the case of local descriptor causing decoder to fail when decoding without using a local table B

--- Test/test_bufr_decode.sh
+++ Test/test_bufr_decode.sh
@@ -3,13 +3,8 @@
 DECODER=../Utilities/bufr_decoder
 export BUFR_TABLES=../Tables/
 
-for i in BUFR/*.bufr
-do
-	BASE=`basename $i .bufr`
-	echo -n "$i ..."
-	OUT=BUFR/${BASE}.out
-	TMPOUT=BUFR/${BASE}.out.tmp
-	${DECODER} -inbufr $i -ltableb local_table_b -no_format > ${TMPOUT} 2>&1
+function test1file 
+{
 	if [ -f ${OUT} ]; then
 		if /usr/bin/diff -q ${OUT} ${TMPOUT} >/dev/null; then
 			echo " (passed)"
@@ -28,6 +23,25 @@ do
 		echo "Renaming ${TMPOUT} to ${OUT}"
 		mv ${TMPOUT} ${OUT}
 	fi
+}
+
+for i in BUFR/*.bufr
+do
+	BASE=`basename $i .bufr`
+	echo -n "$i ..."
+	OUT=BUFR/${BASE}.out
+	TMPOUT=BUFR/${BASE}.out.tmp
+	${DECODER} -inbufr $i -ltableb local_table_b -no_format > ${TMPOUT} 2>&1
+   test1file
 done
+
+# redo the tests when not using local table b
+# Don't think we need to test every file. will test this one only
+i=BUFR/is_winide_BLDU.bufr
+BASE=`basename $i .bufr`
+OUT=BUFR/${BASE}.no_ltb.out
+TMPOUT=BUFR/${BASE}.out.tmp
+${DECODER} -inbufr $i -no_format > ${TMPOUT} 2>&1
+test1file
 
 exit 0
